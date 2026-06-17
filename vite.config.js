3787decbaa5ecc4290b5b56bfdc390ad0017019d
huizhiLLL <2453045@tongji.edu.cn>
2026-06-17 21:12:40 +0800

    del：workers

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), cloudflare()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
